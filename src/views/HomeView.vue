f09702f827f1508586b1da7386f483f551c7411f
Precious Hobobo <precioushobobo@gmail.com>
2024-06-09 22:40:46 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="bg-black h-screen p-20 text-white font-Inter flex justify-center items-center overflow-scroll">
+    <div class="flex flex-col items-center gap-10 w-full ">
+      <div class="w-96 flex p-10 flex-col gap-4 bg-zinc-900 rounded-md justify-center items-center">
+        <img class="w-16 h-16 rounded-full" src="../assets/images/avatar-jessica.jpeg" alt="">
+        <div class="flex flex-col justify-center items-center">
+          <p>Jessica Randall</p>
+          <p class="text-lime900 font-medium">London, United Kingdom</p>
+        </div>
+        <p>"Front-end developer and avid reader."</p>
+        <div class="flex flex-col gap-4 w-full">
+          <a class="flex justify-center items-center p-4 bg-zinc-800 rounded-md font-semibold hover:text-black hover:bg-lime900"
+            href="http://">GitHub</a>
+          <a class="flex justify-center items-center p-4 bg-zinc-800 rounded-md font-semibold hover:text-black hover:bg-lime900"
+            href="http://">Frontend
+            Mentor</a>
+          <a class="flex justify-center items-center p-4 bg-zinc-800 rounded-md font-semibold hover:text-black hover:bg-lime900"
+            href="http://">
+            LinkedIn</a>
+          <a class="flex justify-center items-center p-4 bg-zinc-800 rounded-md font-semibold hover:text-black hover:bg-lime900"
+            href="http://">
+            Twitter</a>
+          <a class="flex justify-center items-center p-4 bg-zinc-800 rounded-md font-semibold hover:text-black hover:bg-lime900"
+            href="http://">
+            Instagram</a>
+        </div>
+
+      </div>
+
+      <div class="">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,html,js}"],
+  theme: {
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      lime900: "#c0e74d",
+    },
+  },
+  plugins: [],
+};
